ecbbe971544e68d34d44375710c4916e986648e9
Steven Tan <steven@steventan.com.au>
2017-09-16 11:56:28 +1000

    project setup

---
+++ client/components/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+/*TODO: import these from node modules*/
+@import "//cdn.rawgit.com/necolas/normalize.css/master/normalize.css";
+@import "//cdn.rawgit.com/milligram/milligram/master/dist/milligram.min.css";
+
+#app {
+  margin: 3rem 0;
+}
+body {
+  font-family: -apple-system, BlinkMacSystemFont,
+               'avenir next', avenir,
+               helvetica, 'helvetica neue',
+               Ubuntu,
+               'segoe ui', arial,
+               sans-serif;
+}
+</style>
---
+++ client/views/Home.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <h5>Electricity production in
+      [region]
+      for the week of
+      [date selector]
+    </h5>
+    <p>charts</p>
+  </div>
+</template>
