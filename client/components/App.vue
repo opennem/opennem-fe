9b226f39c7450375ccbd14f878e92c6f03b72690
Steven Tan <steven@steventan.com.au>
2017-11-10 13:15:06 +1100

    design in progress

--- client/components/App.vue
+++ client/components/App.vue
@@ -5,15 +5,24 @@
 </template>
 
 <style>
-@import url('https://fonts.googleapis.com/css?family=Raleway:700');
+/* @import url('https://fonts.googleapis.com/css?family=Raleway:700'); */
+@import url('https://fonts.googleapis.com/css?family=Merriweather:300,400,700');
 
 body {
-  background-color: #fcfcfc;
-  font-family: -apple-system, BlinkMacSystemFont, 'avenir next', avenir, helvetica, 'helvetica neue', Ubuntu, 'segoe ui', arial, sans-serif;
+  /* fcfcfc */
+  /* background-color: rgba(255, 236, 218, 0.74); */
+  background: #ECE9E6;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to right, #FFFFFF, #ECE9E6);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to right, #FFFFFF, #ECE9E6); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+
+  /* font-family: -apple-system, BlinkMacSystemFont, 'avenir next', avenir, helvetica, 'helvetica neue', Ubuntu, 'segoe ui', arial, sans-serif; */
+  font-family: 'Merriweather', serif;
+}
+select, option {
+  font-family: 'Merriweather', serif;
 }
 
 h2 {
-  font-family: 'Raleway', sans-serif;
   font-weight: 700;
   margin: 0;
   padding: 0 0 10px;
