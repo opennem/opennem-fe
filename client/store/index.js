fc416b1d0f0a6e2c59759d21483c426aeb7524d6
Steven Tan <steven@steventan.com.au>
2018-01-22 21:19:59 +1100

    Clean up vuex store

--- client/store/index.js
+++ client/store/index.js
@@ -1,43 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
-
-import { getJSON } from '../utils/Firebase'
-import { sumRegionsFuelTech, sumFuelTechByRegion, sumDemandByRegion } from '../utils/DataHelpers'
 
 Vue.use(Vuex)
 
 const state = {
   weekStarting: '2017-10-02',
-  regionId: 'all',
-  generationData: null,
-  priceData: null,
-  demandData: [],
-  ftGenData: [],
-  regionFtByGeneratorsData: null,
+  regionId: 'all'
 }
 
 const mutations = {
-  updateWeekStarting(state, data) {
+  updateWeekStarting (state, data) {
     state.weekStarting = data
   },
-  updateRegionId(state, data) {
+  updateRegionId (state, data) {
     state.regionId = data
-  },
-  updateGenerationData(state, data) {
-    state.generationData = data
-  },
-  updatePriceData(state, data) {
-    state.priceData = data
-  },
-  updateDemandData(state, data) {
-    state.demandData = data
-  },
-  updateFtGenData(state, data) {
-    state.ftGenData = data
-  },
-  updateRegionFtByGeneratorsData(state, data) {
-    state.regionFtByGeneratorsData = data
   }
 }
 
@@ -47,100 +23,10 @@ const getters = {
   },
   getRegionId: state => {
     return state.regionId
-  },
-  getGenerationData: state => {
-    return state.generationData
-  },
-  getPriceData: state => {
-    return state.priceData
-  },
-  getDemand: state => {
-    return state.demandData
-  },
-  getFtGen: state => {
-    return state.ftGenData
-  },
-  getRegionFtByGeneratorsData: state => {
-    return state.regionFtByGeneratorsData
   }
 }
 
-const actions = {
-  fetchData({ commit, state }, data) {
-    const week = state.weekStarting
-    const fetchGen = getJSON(`${week}/gen_5m_${data.region}1.json`)
-    const fetchDispatch = getJSON(`${week}/dispatch_5m_${data.region}1.json`)
-    const fetchPrice = getJSON(`${week}/price_30m_${data.region}1.json`)
-
-    axios.all([fetchGen, fetchDispatch, fetchPrice])
-      .then(axios.spread((gen, dispatch, price) => {
-        const data = Object.assign(gen.data, dispatch.data)
-        commit('updateGenerationData', data)
-        commit('updatePriceData', price.data)
-      })
-    )
-  },
-
-  fetchFtGen({ commit, state }, data) {
-    const week = state.weekStarting
-    const fetchNsw = getJSON(`${week}/gen_5m_nsw1.json`)
-    const fetchQld = getJSON(`${week}/gen_5m_qld1.json`)
-    const fetchSa = getJSON(`${week}/gen_5m_sa1.json`)
-    const fetchTas = getJSON(`${week}/gen_5m_tas1.json`)
-    const fetchVic = getJSON(`${week}/gen_5m_vic1.json`)
-
-    axios.all([fetchNsw, fetchQld, fetchSa, fetchTas, fetchVic])
-      .then(axios.spread((nsw, qld, sa, tas, vic) => {
-        const data = sumFuelTechByRegion({
-          'nsw': nsw.data,
-          'qld': qld.data,
-          'sa': sa.data,
-          'tas': tas.data,
-          'vic': vic.data
-        })
-
-        commit('updateFtGenData', data)
-      })
-    )
-  },
-
-  fetchDemand({ commit, state }, data) {
-    const week = state.weekStarting
-    const fetchNsw = getJSON(`${week}/dispatch_5m_nsw1.json`)
-    const fetchQld = getJSON(`${week}/dispatch_5m_qld1.json`)
-    const fetchSa = getJSON(`${week}/dispatch_5m_sa1.json`)
-    const fetchTas = getJSON(`${week}/dispatch_5m_tas1.json`)
-    const fetchVic = getJSON(`${week}/dispatch_5m_vic1.json`)
-
-    axios.all([fetchNsw, fetchQld, fetchSa, fetchTas, fetchVic])
-      .then(axios.spread((nsw, qld, sa, tas, vic) => {
-        const data = sumDemandByRegion({
-          'nsw': nsw.data,
-          'qld': qld.data,
-          'sa': sa.data,
-          'tas': tas.data,
-          'vic': vic.data
-        })
-
-        commit('updateDemandData', data)
-      })
-    )
-  },
-
-  fetchRegionFtByGeneratorsData({ commit, state }, data) {
-    const week = state.weekStarting
-    const getGenerators = getJSON(`${week}/gen_5m_${data.region}1_${data.ft}.json`)
-
-    getGenerators.then(generators => {
-      let res = generators.data
-      if (typeof generators.data === 'string') {
-        res = generators.data.replace(/NaN/g, 'null')
-        res = JSON.parse(res)
-      }
-      commit('updateRegionFtByGeneratorsData', res)
-    })
-  },
-}
+const actions = {}
 
 const store = new Vuex.Store({
   state,
