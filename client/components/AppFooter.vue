cbcd1b0a8dfe7b1e7d95f8ff7b3d0ea8edca1673
Steven Tan <steven@steventan.com.au>
2018-03-02 19:01:32 +1100

    Update footer

--- client/components/AppFooter.vue
+++ client/components/AppFooter.vue
@@ -1,28 +1,33 @@
 <template>
   <footer>
-    <p style="font-size: 1.2em;">OpenNEM is a project of the <a href="http://energy-transition-hub.org/">Energy Transition Hub</a></p>
-
-    <p><small>Data Sources</small><br>
-      <a href="https://www.aemo.com.au/">Australian Energy Market Operator</a><br>
-      <a href="http://www.bom.gov.au/">Bureau of Meteorology</a>
-    </p>
-
-    <p><small>Contact</small><br>
-      <a href="https://twitter.com/OpenNem">@OpenNEM</a><br>
-    </p>
-
-    <!-- Begin MailChimp Signup Form -->
-    <div id="mc_embed_signup">
-      <small>Mailing List</small>
-      <form action="https://opennem.us17.list-manage.com/subscribe/post?u=03d0b7fbc9f8d54c06a410737&amp;id=434dc3a3fb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-        <div id="mc_embed_signup_scroll">
-          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
-          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_03d0b7fbc9f8d54c06a410737_434dc3a3fb" tabindex="-1" value=""></div>
-          <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-        </div>
-      </form>
+    <div style="margin-bottom: 2rem;">
+      
+      <small>Sources:</small>
+      <a href="https://www.aemo.com.au/">AEMO</a>
+      <a href="http://www.bom.gov.au/">BOM</a>
+      
     </div>
+
+    <div style="text-align: left;">
+      <div style="font-size: 1.2em;">OpenNEM is a project of the <a href="http://energy-transition-hub.org/">Energy Transition Hub</a></div>
+        
+      <!-- <div style="margin: 1rem 0;"><small>Contact</small><br><a href="https://twitter.com/OpenNem">@OpenNEM</a></div> -->
+
+      <!-- Begin MailChimp Signup Form -->
+      <div id="mc_embed_signup">
+        <form action="https://opennem.us17.list-manage.com/subscribe/post?u=03d0b7fbc9f8d54c06a410737&amp;id=434dc3a3fb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <div id="mc_embed_signup_scroll">
+            <label for=""><small>Mailing List</small></label>
+            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email" required>
+            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_03d0b7fbc9f8d54c06a410737_434dc3a3fb" tabindex="-1" value=""></div>
+            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+          </div>
+        </form>
+      </div>
+    </div>
+
+    
   </footer>
 </template>
 
@@ -33,27 +38,24 @@ export default {};
 <style scoped>
 footer {
   border-top: 1px solid rgba(199, 69, 35, 0.4);
-  background: rgba(199, 69, 35, 0.05);
+  /* background: rgba(199, 69, 35, 0.05); */
   font-size: 0.8em;
   margin: 4rem 0 0;
   padding: 1.5rem 2rem;
+  color: #666;
 }
-p {
-  margin-bottom: 1.5rem;
-  line-height: 1.2rem;
+small {
+  /* font-style: italic; */
+  font-weight: 600;
+}
 
-  small {
-    /* font-style: italic; */
-    font-weight: 400;
-  }
-
-  a {
-    font-style: normal;
-    font-weight: 600;
-  }
+a {
+  font-style: normal;
+  font-weight: 600;
 }
 
 #mc_embed_signup {
+  margin: 2rem 0 1rem;
   small {
     display: block;
     margin-bottom: 3px;
@@ -78,7 +80,10 @@ p {
 
 @media only screen and (min-width: 960px) {
   footer {
-    padding: 1.5rem 5rem;
+    padding: 1.5rem 1rem;
+    display: flex;
+    justify-content: space-between;
+    flex-flow: row wrap;
   }
 }
 </style>
