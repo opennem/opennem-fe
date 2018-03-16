ed986da77a231a06871a71ff30b0b036c1b4ce27
Steven Tan <steven@steventan.com.au>
2018-03-16 19:01:52 +1100

    Updated About page and footer

--- client/components/AppFooter.vue
+++ client/components/AppFooter.vue
@@ -2,32 +2,17 @@
   <footer>
     <div style="margin-bottom: 2rem;">
       
-      <small>Sources:</small>
+      <span>Sources:</span>
       <a href="https://www.aemo.com.au/">AEMO</a>,
       <a href="http://www.bom.gov.au/">BoM</a>
       
     </div>
 
     <div style="text-align: left;">
-      <div style="font-size: 1.2em;">OpenNEM is a project of the <a href="http://energy-transition-hub.org/">Energy Transition Hub</a></div>
+      <div>OpenNEM is a project of the <a href="http://energy-transition-hub.org/">Energy Transition Hub</a></div>
         
       <!-- <div style="margin: 1rem 0;"><small>Contact</small><br><a href="https://twitter.com/OpenNem">@OpenNEM</a></div> -->
-
-      <!-- Begin MailChimp Signup Form -->
-      <div id="mc_embed_signup">
-        <form action="https://opennem.us17.list-manage.com/subscribe/post?u=03d0b7fbc9f8d54c06a410737&amp;id=434dc3a3fb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-          <div id="mc_embed_signup_scroll">
-            <label for=""><small>Mailing List</small></label>
-            <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email" required>
-            <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-            <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_03d0b7fbc9f8d54c06a410737_434dc3a3fb" tabindex="-1" value=""></div>
-            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
-          </div>
-        </form>
-      </div>
     </div>
-
-    
   </footer>
 </template>
 
@@ -37,8 +22,9 @@ export default {};
 
 <style scoped>
 footer {
-  border-top: 1px solid rgba(199, 69, 35, 0.4);
+  /* border-top: 1px dashed rgba(199, 69, 35, 0.6); */
   /* background: rgba(199, 69, 35, 0.05); */
+  border-top: 1px solid rgba(3,3,3, 0.1);
   font-size: 0.8em;
   margin: 4rem 0 0;
   padding: 1.5rem 2rem;
@@ -54,29 +40,7 @@ a {
   font-weight: 600;
 }
 
-#mc_embed_signup {
-  margin: 2rem 0 1rem;
-  small {
-    display: block;
-    margin-bottom: 3px;
-  }
-}
-#mce-EMAIL {
-  padding: 0.3rem 0.5rem;
-  border: none;
-  border-radius: 4px 0 0 4px;
-}
-#mc-embedded-subscribe {
-  border: none;
-  padding: 0.3rem 0.5rem;
-  border-radius: 0 4px 4px 0;
-  position: relative;
-  left: -2px;
 
-  &:hover {
-    background-color: #eee;
-  }
-}
 
 @media only screen and (min-width: 960px) {
   footer {
--- client/views/About.vue
+++ client/views/About.vue
@@ -20,7 +20,22 @@
         By 
         <a href="https://twitter.com/dylanjmcconnell" title="Dylan's twitter page" rel="external">Dylan McConnell</a>, 
         <a href="https://twitter.com/simonahac" title="Simon's twitter page" rel="external">Simon Holmes à Court</a> and
-        <a href="https://twitter .com/chienleng" title="Steven's twitter page" rel="external">Steven Tan</a></p>
+        <a href="https://twitter.com/chienleng" title="Steven's twitter page" rel="external">Steven Tan</a>
+      </p>
+    </section>
+
+    <h2>How to keep in touch</h2>
+    <section>
+      <div>Follow us on <a href="https://twitter.com/opennem" title="OpenNEM's twitter page" rel="external">@OpenNEM</a>. <br>
+        We also send an occasional email:
+        <form style="display: inline; margin-left: 5px; white-space: nowrap" action="https://opennem.us17.list-manage.com/subscribe/post?u=03d0b7fbc9f8d54c06a410737&amp;id=434dc3a3fb" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="your email" required>
+          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_03d0b7fbc9f8d54c06a410737_434dc3a3fb" tabindex="-1" value=""></div>
+          <input type="submit" value="join" name="subscribe" id="mc-embedded-subscribe" class="button">
+        </form>
+      </div>
+      
     </section>
   </div>
 </template>
@@ -33,9 +48,17 @@ export default {}
 h1 {
   font-size: 1.7rem;
   line-height: 2.2rem;
-  margin-bottom: 2rem;
+  margin: 3rem 0 2rem;
   font-weight: 200;
   color: #3a3a3a;
+}
+h2 {
+  font-size: 1.5rem;
+  font-weight: 200;
+  color: #3a3a3a;
+  /* border-top: 1px dashed #bbb; */
+  margin-top: 3rem;
+  padding-top: 3rem;
 }
 .article {
   padding: 0 2rem;
@@ -44,18 +67,34 @@ h1 {
 section {
   font-size: 0.9rem;
   line-height: 1.6rem;
-  color: #3a3a3a;
+  color: #5a5a5a;
 
   strong {
     color: #000;
+    font-size: 1.2em;
   }
 }
 
 p.credits {
   font-size: 0.85em; 
-  border-top: 1px dashed #bbb; 
   margin-top: 1.7rem; 
-  padding-top: 1.7rem;
+}
+
+#mce-EMAIL {
+  padding: 0.3rem 0.5rem;
+  border: none;
+  border-radius: 4px 0 0 4px;
+}
+#mc-embedded-subscribe {
+  border: none;
+  padding: 0.3rem 0.5rem;
+  border-radius: 0 4px 4px 0;
+  position: relative;
+  left: -2px;
+
+  &:hover {
+    background-color: #eee;
+  }
 }
 
 @media only screen and (min-width: 960px) {
