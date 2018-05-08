972b59073105bbb49a11b9e166f64395e5e4633f
Steven Tan <chienleng@gmail.com>
2018-05-08 12:11:01 +1000

    Show % of contribution in Legend

--- src/components/Export/Legend.vue
+++ src/components/Export/Legend.vue
@@ -2,13 +2,19 @@
   <section>
     <div class="graph" v-for="row in rangeSummary.sourcesData" :key="row.id">
       <span class="source-colour" :style="{ backgroundColor: row.colour }"></span>
-      <span class="source-label">{{row.label}}</span>
+      <div class="source-label">
+        {{row.label}}
+        <em>
+          ({{ getContribution(row.range.power, rangeSummary.totalGrossPower) | formatNumber }}<span v-if="hasValue(getContribution(row.range.power, rangeSummary.totalGrossPower))">%</span>)
+        </em>
+      </div>
     </div>
   </section>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
+import { formatNumberForDisplay } from '@/lib/formatter';
 
 export default {
   computed: {
@@ -16,22 +22,52 @@ export default {
       rangeSummary: 'getRangeSummary',
     }),
   },
+  methods: {
+    hasValue(value) {
+      return value || false;
+    },
+    getContribution(pointValue, total) {
+      return (pointValue / total) * 100;
+    },
+  },
+  filters: {
+    formatNumber(value, format) {
+      return formatNumberForDisplay(value, format);
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+@import "../../../node_modules/bulma/sass/utilities/mixins.sass";
+
 section {
   margin-top: 0;
   padding: 0 1rem;
+  display: flex;
+  flex-wrap: wrap;
 }
 .graph {
-  display: inline-block;
   font-size: 0.75rem;
   margin-right: 0.5rem;
+  width: 47%;
+
+  @include desktop {
+    width: 31%;
+  }
+}
+.source-label {
+  display: inline-block;
+  font-weight: 600;
+
+  em {
+    font-weight: normal;
+    font-style: normal;
+  }
 }
 .source-colour {
-  width: 13px;
-  height: 13px;
+  width: 14px;
+  height: 14px;
   background-color: rgba(255,255,255,.8);
   display: inline-block;
   vertical-align: text-bottom;
