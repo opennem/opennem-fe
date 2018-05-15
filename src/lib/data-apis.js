869a7d98a19ce53698639a7584a04acfec709912
Steven Tan <chienleng@gmail.com>
2018-05-15 18:40:16 +1000

    Catch api call errors

--- src/lib/data-apis.js
+++ src/lib/data-apis.js
@@ -5,17 +5,21 @@ const http = axios.create({
 });
 
 function fetchJSON(ref) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     http.get(ref).then((data) => {
       resolve(data);
+    }).catch((e) => {
+      reject(e);
     });
   });
 }
 
 export default function (ref) {
-  return new Promise((resolve) => {
+  return new Promise((resolve, reject) => {
     fetchJSON(ref).then((data) => {
       resolve(data);
+    }).catch((e) => {
+      reject(e);
     });
   });
 }
