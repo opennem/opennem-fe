e52cde37c7796a6c913384947e565010946c959b
Steven Tan <steven@steventan.com.au>
2018-05-23 16:25:21 +1000

    Update period names

--- src/contants/periods.js
+++ src/contants/periods.js
@@ -1,3 +1,3 @@
-export const PERIODS_5_MIN = '5mm';
-export const PERIODS_30_MINS = '30mm';
-export const PERIODS_1_DAY = 'DD';
+export const PERIOD_5_MIN = '5mm';
+export const PERIOD_30_MINS = '30mm';
+export const PERIOD_1_DAY = 'DD';
