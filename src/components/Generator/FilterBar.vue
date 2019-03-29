fd3554a993b5994874ff51cb1599f70725987e65
Steven Tan <steven@steventan.com.au>
2019-03-29 12:50:24 +1100

    Update filter by technology

--- src/components/Generator/FilterBar.vue
+++ src/components/Generator/FilterBar.vue
@@ -10,12 +10,12 @@
     <div class="dropdown" :class="{'is-active': techDropdownActive}" v-on-clickaway="onClickAway">
       <button
         class="dropdown-trigger button is-rounded is-small is-primary"
-        :class="{ 'is-inverted': selectedTechs.length === 0 }"
+        :class="{ 'is-inverted': selectedTechGroups.length === 0 }"
         @click="techDropdownActive = true"
       >
         <div class="dropdown-label">
           <span>Technology</span>
-          <strong v-if="selectedTechs.length > 0">({{selectedTechs.length}})</strong>
+          <strong v-if="selectedTechGroups.length > 0">({{selectedTechGroups.length}})</strong>
         </div>
         <font-awesome-icon class="fal" :icon="iconDown" />
       </button>
@@ -23,14 +23,39 @@
       <transition name="slide-down-fade">
         <div v-if="techDropdownActive" class="dropdown-menu">
           <div class="dropdown-content">
-            <a class="dropdown-item" v-for="(d, i) in allTechs" :key="i" @click="handleTechClick(d.id)">
-              <span class="source-colour"
-                :style="{ 
-                  backgroundColor: isSelected(d.id) ? d.colour : '#fff',
-                  'border-color': d.colour
-                }"><font-awesome-icon class="checkmark-icon fal" :icon="iconCheckmark" /></span>
-              {{d.label}}
-            </a>
+            <div class="dropdown-item" v-for="(d, i) in allTechs" :key="i">
+
+              <div class="item-group">
+                <span class="expand-collapse" @click="handleExpandCollapseClick(d.id)">
+                  <font-awesome-icon class="fal fa-fw" v-if="d.fields.length > 1 && isGroupExpanded(d.id)" :icon="iconCaretDown" />
+                  <font-awesome-icon class="fal fa-fw" v-if="d.fields.length > 1 && !isGroupExpanded(d.id)" :icon="iconCaretRight" />
+                </span>
+                <span @click="handleTechGroupClick(d)">
+                  <span class="source-colour"
+                    :style="{ 
+                      backgroundColor: isGroupSelected(d.id) ? d.colour : '#fff',
+                      'border-color': d.colour
+                    }">
+                      <font-awesome-icon v-if="hasSelection(d)" class="checkmark-icon fal" :icon="iconMinus" />
+                      <font-awesome-icon v-else class="checkmark-icon fal" :icon="iconCheckmark" />
+                    </span>
+                  {{d.label}}
+                </span>
+              </div>
+              
+              <div class="subitem-group" v-show="d.fields.length > 1 && isGroupExpanded(d.id)" >
+                <div v-for="(field, fIndex) in d.fields" :key="fIndex">
+                  <span @click="handleTechClick(d, field)">
+                    <span class="source-colour"
+                      :style="{ 
+                        backgroundColor: isTechSelected(field) ? getTechColour(field) : '#fff',
+                        'border-color': getTechColour(field)
+                      }"><font-awesome-icon class="checkmark-icon fal" :icon="iconCheckmark" /></span>
+                    {{getTechLabel(field)}}
+                  </span>
+                </div> 
+              </div>
+            </div>
 
             <div class="buttons">
               <a class="button is-rounded is-small is-inverted" @click="clearSelectedTechs">Clear</a>
@@ -48,10 +73,11 @@
 <script>
 import _ from 'lodash';
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome';
-import { faAngleDown, faCheck } from '@fortawesome/fontawesome-pro-light';
+import { faAngleDown, faCheck, faCaretRight, faCaretDown, faMinus } from '@fortawesome/fontawesome-pro-light';
 import { mixin as clickaway } from 'vue-clickaway';
 import EventBus from '@/lib/event-bus';
 import { GraphDomains } from '@/domains/graphs';
+import Simplified from '@/domains/groups/simplified';
 
 export default {
   mixins: [clickaway],
@@ -64,6 +90,9 @@ export default {
       techDropdownActive: false,
       allTechs: [],
       selectedTechs: [],
+      selectedTechGroups: [],
+      groupExpanded: [],
+      simplifiedGroup: _.cloneDeep(Simplified.groups),
     }
   },
   computed: {
@@ -72,6 +101,15 @@ export default {
     },
     iconCheckmark() {
       return faCheck;
+    },
+    iconMinus() {
+      return faMinus;
+    },
+    iconCaretRight() {
+      return faCaretRight;
+    },
+    iconCaretDown() {
+      return faCaretDown;
     },
   },
   watch: {
@@ -85,13 +123,24 @@ export default {
   mounted() {
     EventBus.$on('generators.filter.clear', this.clearFilter);
 
-    Object.keys(GraphDomains).forEach(d => {
-      if (GraphDomains[d].type === 'sources' && d !== 'imports') {
-        const source = GraphDomains[d];
-        source.id = d;
-        this.allTechs.push(source);
-      }
-    });
+    const groups = this.simplifiedGroup.filter(g => g.type === 'sources' && g.id !== 'imports');
+    const findSolarGroup = groups.find(g => g.id === 'group_solar');
+    const findBatterGroup = groups.find(g => g.id === 'group_battery_discharging');
+
+    findSolarGroup.fields = ['solar']; // remove 'rooftop_solar'
+    findBatterGroup.label = 'Battery'; // rename battery discharging
+
+    groups.forEach(g => {
+      this.allTechs.push(g);
+
+      g.fields.forEach(f => {
+        if (_.includes(this.selectedTechs, f) &&
+          !_.includes(this.selectedTechGroups, g.id)) {
+          this.selectedTechGroups.push(g.id);
+        }
+      })
+    })
+    
     this.allTechs.reverse();
   },
   beforeDestroy() {
@@ -101,14 +150,53 @@ export default {
     onClickAway() {
       this.techDropdownActive = false;
     },
-    isSelected(techId) {
+    isGroupSelected(groupId) {
+      return _.includes(this.selectedTechGroups, groupId);
+    },
+    isTechSelected(techId) {
       return _.includes(this.selectedTechs, techId);
+    },
+    isGroupExpanded(groupId) {
+      return _.includes(this.groupExpanded, groupId);
+    },
+    hasSelection(group) {
+      const fieldsLength = group.fields.length;
+      let fieldsSelected = 0;
+      group.fields.forEach(f => {
+        if (_.includes(this.selectedTechs, f)) {
+          fieldsSelected += 1;
+        }
+      });
+      return fieldsSelected !== fieldsLength;
+    },
+    getTechLabel(techId) {
+      return GraphDomains[techId].label;
+    },
+    getTechColour(techId) {
+      return GraphDomains[techId].colour;
     },
     emitSelectedTechs() {
       this.$emit('selected', this.selectedTechs);
       this.$store.dispatch('generatorsSelectedTechs', this.selectedTechs);
     },
-    handleTechClick(techId) {
+    handleTechGroupClick(group) {
+      const groupId = group.id;
+      const isGroupIncluded = _.includes(this.selectedTechGroups, groupId);
+      if (isGroupIncluded) {
+        this.selectedTechGroups = this.selectedTechGroups.filter(d => d !== groupId);
+        group.fields.forEach(f => {
+          this.selectedTechs = this.selectedTechs.filter(d => d !== f);
+        });
+      } else {
+        this.selectedTechGroups.push(groupId);
+        group.fields.forEach(f => {
+          this.selectedTechs.push(f);
+        });
+      }
+
+      this.emitSelectedTechs();
+    },
+    handleTechClick(group, techId) {
       const isIncluded = _.includes(this.selectedTechs, techId);
       if (isIncluded) {
         this.selectedTechs = this.selectedTechs.filter(d => d !== techId);
@@ -116,9 +204,36 @@ export default {
         this.selectedTechs.push(techId);
       }
 
+      const fieldsLength = group.fields.length;
+      let fieldsSelected = 0;
+      group.fields.forEach(f => {
+        if (_.includes(this.selectedTechs, f)) {
+          fieldsSelected += 1;
+        }
+      });
+
+      if (fieldsSelected === 0) {
+        this.selectedTechGroups = this.selectedTechGroups.filter(d => d !== group.id);
+      } else if (fieldsSelected !== 0) {
+        const isGroupIncluded = _.includes(this.selectedTechGroups, group.id);
+        if (!isGroupIncluded) {
+          this.selectedTechGroups.push(group.id);
+        }
+      }
+
       this.emitSelectedTechs();
     },
+    handleExpandCollapseClick(groupId) {
+      const isIncluded = _.includes(this.groupExpanded, groupId);
+      if (isIncluded) {
+        this.groupExpanded = this.groupExpanded.filter(d => d !== groupId);
+      } else {
+        this.groupExpanded.push(groupId);
+      }
+      console.log(this.groupExpanded)
+    },
     clearSelectedTechs() {
+      this.selectedTechGroups = [];
       this.selectedTechs = [];
       this.emitSelectedTechs();
     },
@@ -143,7 +258,25 @@ export default {
 
   .dropdown-item {
     font-size: 11px;
-    padding: 0.25rem 0.6rem
+    padding: 0.25rem 0.4rem;
+    cursor: pointer;
+    user-select: none;
+    
+    .item-group {
+      display: flex;
+    }
+
+    .subitem-group {
+      margin: 4px 0 0;
+      padding: 2px 19px;
+      background-color: #efefef;
+      box-shadow: inset 1px 1px 4px rgba(100, 100, 100, 0.1);
+      border-radius: 2px;
+
+      div {
+        padding: 2px 0;
+      }
+    }
   }
 }
 .filter-bar {
@@ -164,6 +297,13 @@ export default {
     }
   }
 }
+.expand-collapse {
+  font-size: 13px;
+  display: block;
+  width: 19px;
+  height: 15px;
+  position: relative;
+}
 .source-colour {
   width: 15px;
   height: 15px;
@@ -179,7 +319,7 @@ export default {
   .checkmark-icon {
     position: relative;
     left: 2px;
-    bottom: 1px;
+    bottom: 0;
     color: #fff;
   }
 }
