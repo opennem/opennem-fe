819c5e3e526a91054bb986d88eec4ec1c277719b
Steven Tan <steven@steventan.com.au>
2019-11-25 15:20:40 +0700

    Refactor feature toggle code

--- layouts/main.vue
+++ layouts/main.vue
@@ -7,27 +7,14 @@
 </template>
 
 <script>
-import { lsGet, lsSet } from '~/services/LocalStorage'
 import AppHeader from '~/components/layout/AppHeader'
 import AppFooter from '~/components/layout/AppFooter'
 
-const MutationTypes = {
-  FEATURE_TOGGLE_EMISSIONS: 'FEATURE_TOGGLE_EMISSIONS'
-}
 export default {
   middleware: 'meta',
   components: {
     AppHeader,
     AppFooter
-  },
-  mounted() {
-    const featureEmissions = lsGet(MutationTypes.FEATURE_TOGGLE_EMISSIONS)
-    // set up local storage
-    if (featureEmissions) {
-      this.$store.dispatch('featureEmissions', featureEmissions)
-    } else {
-      lsSet(MutationTypes.FEATURE_TOGGLE_EMISSIONS, false)
-    }
   }
 }
 </script>
