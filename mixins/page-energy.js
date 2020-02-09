2673cd8aeb84a6646048548332998a4f4612c2f7
Steven Tan <steven@steventan.com.au>
2020-02-09 12:23:32 +1100

    fix issue where EI calculation did not take into account of TWh

---
+++ mixins/page-energy.js
@@ -0,0 +1,116 @@
+const pageEnergyMixin = {
+  created() {
+    console.log('mixin!')
+  },
+  methods: {
+    updateYMinMax() {
+      const isGeneration = this.percentContributionTo === 'generation'
+      const emissionsIntensityDataset = [],
+        emissionsVolumeDataset = []
+      let energyMinAll = 0,
+        energyMaxAll = 0,
+        emissionsMinAll = 0,
+        emissionsMaxAll = 0,
+        emissionsIntensityMinAll = 0,
+        emissionsIntensityMaxAll = 1200
+
+      this.dataset.forEach((d, i) => {
+        let totalDemand = 0,
+          totalGeneration = 0,
+          totalEmissionsVol = 0,
+          energyMin = 0,
+          energyMax = 0,
+          emissionsMin = 0,
+          emissionsMax = 0
+
+        this.stackedAreaDomains.forEach(domain => {
+          const id = domain.id
+          totalDemand += d[id] || 0
+          if (domain.category == 'source' && domain.fuelTech !== 'imports') {
+            totalGeneration += d[id] || 0
+          }
+          energyMax += d[id] || 0
+          if (d[id] < 0) {
+            energyMin += d[id] || 0
+          }
+        })
+
+        if (energyMax > energyMaxAll) {
+          energyMaxAll = energyMax
+        }
+        if (energyMin < energyMinAll) {
+          energyMinAll = energyMin
+        }
+
+        emissionsVolumeDataset.push({
+          date: d.date
+        })
+        const lastEVIndex = emissionsVolumeDataset.length - 1
+
+        this.emissionStackedAreaDomains.forEach(domain => {
+          const id = domain.id
+
+          if (
+            !isGeneration ||
+            (isGeneration &&
+              domain.fuelTech !== 'imports' &&
+              domain.fuelTech !== 'exports')
+          ) {
+            totalEmissionsVol += d[id] || 0
+            emissionsMax += d[id] || 0
+
+            if (d[id] < 0) {
+              emissionsMin += d[id] || 0
+            }
+
+            emissionsVolumeDataset[lastEVIndex][id] = d[id]
+          }
+        })
+
+        if (emissionsMax > emissionsMaxAll) {
+          emissionsMaxAll = emissionsMax
+        }
+        if (emissionsMin < emissionsMinAll) {
+          emissionsMinAll = emissionsMin
+        }
+
+        let ei = isGeneration
+          ? totalEmissionsVol / totalGeneration
+          : totalEmissionsVol / totalDemand
+        if (this.interval === 'Year' || this.interval === 'Fin Year') {
+          ei = ei / 1000
+        }
+        const isValidEI = Number.isFinite(ei)
+        if (isValidEI && ei > emissionsIntensityMaxAll) {
+          emissionsIntensityMaxAll = ei
+        }
+        if (isValidEI && ei < emissionsIntensityMinAll) {
+          emissionsIntensityMinAll = ei
+        }
+        emissionsIntensityDataset.push({
+          date: d.date,
+          _emissionsIntensity: isValidEI ? ei : null
+        })
+      })
+
+      // duplicate last valid EV value to render the step line correctly
+      const evDatasetLength = emissionsIntensityDataset.length
+      const lastValidEI =
+        emissionsIntensityDataset[evDatasetLength - 2]._emissionsIntensity
+      emissionsIntensityDataset[
+        evDatasetLength - 1
+      ]._emissionsIntensity = lastValidEI
+
+      this.energyMin = energyMinAll
+      this.energyMax = energyMaxAll
+      this.emissionsMin = emissionsMinAll
+      this.emissionsMax = emissionsMaxAll
+      this.emissionsIntensityDataset = emissionsIntensityDataset
+      this.emissionsIntensityMin = emissionsIntensityMinAll
+      this.emissionsIntensityMax = emissionsIntensityMaxAll
+      this.emissionsVolumeDataset = emissionsVolumeDataset
+    }
+  }
+}
+
+export default pageEnergyMixin
--- pages/energy/_region/index.vue
+++ pages/energy/_region/index.vue
@@ -613,6 +613,7 @@ import _cloneDeep from 'lodash.clonedeep'
 import Draggable from 'vuedraggable'
 import { saveAs } from 'file-saver'
 
+import PageEnergyMixin from '~/mixins/page-energy.js'
 import REGIONS from '~/constants/regions.js'
 import { EMISSIONS } from '~/constants/emissions.js'
 import EventBus from '~/plugins/eventBus.js'
@@ -682,6 +683,8 @@ export default {
     EnergyRecords,
     EnergyCompare
   },
+
+  mixins: [PageEnergyMixin],
 
   data() {
     return {
@@ -1620,111 +1623,6 @@ export default {
       } else {
         this.filteredDataset = dataset
       }
-    },
-
-    updateYMinMax() {
-      const isGeneration = this.percentContributionTo === 'generation'
-      const emissionsIntensityDataset = [],
-        emissionsVolumeDataset = []
-      let energyMinAll = 0,
-        energyMaxAll = 0,
-        emissionsMinAll = 0,
-        emissionsMaxAll = 0,
-        emissionsIntensityMinAll = 0,
-        emissionsIntensityMaxAll = 1200
-
-      this.dataset.forEach((d, i) => {
-        let totalDemand = 0,
-          totalGeneration = 0,
-          totalEmissionsVol = 0,
-          energyMin = 0,
-          energyMax = 0,
-          emissionsMin = 0,
-          emissionsMax = 0
-
-        this.stackedAreaDomains.forEach(domain => {
-          const id = domain.id
-          totalDemand += d[id] || 0
-          if (domain.category == 'source' && domain.fuelTech !== 'imports') {
-            totalGeneration += d[id] || 0
-          }
-          energyMax += d[id] || 0
-          if (d[id] < 0) {
-            energyMin += d[id] || 0
-          }
-        })
-
-        if (energyMax > energyMaxAll) {
-          energyMaxAll = energyMax
-        }
-        if (energyMin < energyMinAll) {
-          energyMinAll = energyMin
-        }
-
-        emissionsVolumeDataset.push({
-          date: d.date
-        })
-        const lastEVIndex = emissionsVolumeDataset.length - 1
-
-        this.emissionStackedAreaDomains.forEach(domain => {
-          const id = domain.id
-
-          if (
-            !isGeneration ||
-            (isGeneration &&
-              domain.fuelTech !== 'imports' &&
-              domain.fuelTech !== 'exports')
-          ) {
-            totalEmissionsVol += d[id] || 0
-            emissionsMax += d[id] || 0
-
-            if (d[id] < 0) {
-              emissionsMin += d[id] || 0
-            }
-
-            emissionsVolumeDataset[lastEVIndex][id] = d[id]
-          }
-        })
-
-        if (emissionsMax > emissionsMaxAll) {
-          emissionsMaxAll = emissionsMax
-        }
-        if (emissionsMin < emissionsMinAll) {
-          emissionsMinAll = emissionsMin
-        }
-
-        const ei = isGeneration
-          ? totalEmissionsVol / totalGeneration
-          : totalEmissionsVol / totalDemand
-        const isValidEI = Number.isFinite(ei)
-        if (isValidEI && ei > emissionsIntensityMaxAll) {
-          emissionsIntensityMaxAll = ei
-        }
-        if (isValidEI && ei < emissionsIntensityMinAll) {
-          emissionsIntensityMinAll = ei
-        }
-        emissionsIntensityDataset.push({
-          date: d.date,
-          _emissionsIntensity: isValidEI ? ei : null
-        })
-      })
-
-      // duplicate last valid EV value to render the step line correctly
-      const evDatasetLength = emissionsIntensityDataset.length
-      const lastValidEI =
-        emissionsIntensityDataset[evDatasetLength - 2]._emissionsIntensity
-      emissionsIntensityDataset[
-        evDatasetLength - 1
-      ]._emissionsIntensity = lastValidEI
-
-      this.energyMin = energyMinAll
-      this.energyMax = energyMaxAll
-      this.emissionsMin = emissionsMinAll
-      this.emissionsMax = emissionsMaxAll
-      this.emissionsIntensityDataset = emissionsIntensityDataset
-      this.emissionsIntensityMin = emissionsIntensityMinAll
-      this.emissionsIntensityMax = emissionsIntensityMaxAll
-      this.emissionsVolumeDataset = emissionsVolumeDataset
     },
 
     updateCompare(dataset) {
