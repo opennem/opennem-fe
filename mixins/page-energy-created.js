2a8022ff362f40a1cb423cd7b8cf6678a7e1116a
Steven Tan <steven@steventan.com.au>
2020-03-29 16:19:02 +1100

    move data() and mounted() to mixin

--- mixins/page-energy-created.js
+++ mixins/page-energy-created.js
@@ -1,4 +1,5 @@
 import EventBus from '~/plugins/eventBus.js'
+import _debounce from 'lodash.debounce'
 
 const pageEnergyCreated = {
   head() {
@@ -34,12 +35,93 @@ const pageEnergyCreated = {
     }
   },
 
+  data() {
+    return {
+      mounted: false,
+      ready: false,
+      originalDataset: [],
+      dataset: [],
+      energyDomains: [],
+      fuelTechEnergyOrder: [],
+      emissionsOrder: [],
+      marketValueDomains: [],
+      temperatureDomains: [],
+      temperatureMeanId: '',
+      temperatureMinId: '',
+      temperatureMaxId: '',
+      priceDomains: [],
+      emissionDomains: [],
+      responses: [],
+      hoverDate: null,
+      hoverDomain: null,
+      hoverEmissionVolumeDomain: null,
+      focusDate: null,
+      mouseLoc: null,
+      tooltipLeft: 0,
+      filteredDataset: [],
+      visHeight: 0,
+      hoverOn: false,
+      lineColour: '#e34a33',
+      windowWidth: 0,
+      energyMin: 0,
+      energyMax: 1000,
+      emissionsVolumeDataset: [],
+      emissionsMin: 0,
+      emissionsMax: 1000,
+      emissionsIntensityDataset: [],
+      emissionsIntensityMin: 0,
+      emissionsIntensityMax: 1000,
+      isTouchDevice: false,
+      compareData: [],
+      summary: null,
+      renewablesPercentageDataset: []
+    }
+  },
+
   created() {
     this.$store.dispatch('currentView', 'energy')
     EventBus.$on('dataset.filter', this.handleDatasetFilter)
     EventBus.$on('vis.mousemove', this.handleVisMouseMove)
     EventBus.$on('vis.mouseenter', this.handleVisEnter)
     EventBus.$on('vis.mouseleave', this.handleVisLeave)
+  },
+
+  mounted() {
+    function is_touch_device() {
+      var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ')
+      var mq = function(query) {
+        return window.matchMedia(query).matches
+      }
+
+      if (
+        'ontouchstart' in window ||
+        (window.DocumentTouch && document instanceof DocumentTouch)
+      ) {
+        return true
+      }
+
+      // include the 'heartz' as a way to have a non matching MQ to help terminate the join
+      // https://git.io/vznFH
+      var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join(
+        ''
+      )
+      return mq(query)
+    }
+
+    this.isTouchDevice = is_touch_device()
+    this.windowWidth = window.innerWidth
+    this.visHeight = this.widthBreak ? 578 : 350
+    this.$nextTick(() => {
+      window.addEventListener(
+        'resize',
+        _debounce(() => {
+          this.windowWidth = window.innerWidth
+          this.visHeight = this.widthBreak ? 578 : 350
+        }, 200)
+      )
+    })
+    this.fetchData(this.regionId, this.range)
+    this.mounted = true
   },
 
   beforeDestroy() {
--- pages/energy/_region/index.vue
+++ pages/energy/_region/index.vue
@@ -660,7 +660,6 @@ import moment from 'moment'
 import { timeFormat as d3TimeFormat } from 'd3-time-format'
 import { mouse as d3Mouse } from 'd3-selection'
 import { extent as d3Extent, max as d3Max } from 'd3-array'
-import _debounce from 'lodash.debounce'
 import _includes from 'lodash.includes'
 import _cloneDeep from 'lodash.clonedeep'
 import Draggable from 'vuedraggable'
@@ -709,49 +708,6 @@ export default {
   },
 
   mixins: [PageEnergyMixin, PerfLogMixin, PageEnergyCreatedMixin],
-
-  data() {
-    return {
-      mounted: false,
-      ready: false,
-      originalDataset: [],
-      dataset: [],
-      energyDomains: [],
-      fuelTechEnergyOrder: [],
-      emissionsOrder: [],
-      marketValueDomains: [],
-      temperatureDomains: [],
-      temperatureMeanId: '',
-      temperatureMinId: '',
-      temperatureMaxId: '',
-      priceDomains: [],
-      emissionDomains: [],
-      responses: [],
-      hoverDate: null,
-      hoverDomain: null,
-      hoverEmissionVolumeDomain: null,
-      focusDate: null,
-      mouseLoc: null,
-      tooltipLeft: 0,
-      filteredDataset: [],
-      visHeight: 0,
-      hoverOn: false,
-      lineColour: '#e34a33',
-      windowWidth: 0,
-      energyMin: 0,
-      energyMax: 1000,
-      emissionsVolumeDataset: [],
-      emissionsMin: 0,
-      emissionsMax: 1000,
-      emissionsIntensityDataset: [],
-      emissionsIntensityMin: 0,
-      emissionsIntensityMax: 1000,
-      isTouchDevice: false,
-      compareData: [],
-      summary: null,
-      renewablesPercentageDataset: []
-    }
-  },
 
   computed: {
     ...mapGetters({
@@ -1407,44 +1363,6 @@ export default {
         this.$store.dispatch('compareDates', [])
       }
     }
-  },
-
-  mounted() {
-    function is_touch_device() {
-      var prefixes = ' -webkit- -moz- -o- -ms- '.split(' ')
-      var mq = function(query) {
-        return window.matchMedia(query).matches
-      }
-
-      if (
-        'ontouchstart' in window ||
-        (window.DocumentTouch && document instanceof DocumentTouch)
-      ) {
-        return true
-      }
-
-      // include the 'heartz' as a way to have a non matching MQ to help terminate the join
-      // https://git.io/vznFH
-      var query = ['(', prefixes.join('touch-enabled),('), 'heartz', ')'].join(
-        ''
-      )
-      return mq(query)
-    }
-
-    this.isTouchDevice = is_touch_device()
-    this.windowWidth = window.innerWidth
-    this.visHeight = this.widthBreak ? 578 : 350
-    this.$nextTick(() => {
-      window.addEventListener(
-        'resize',
-        _debounce(() => {
-          this.windowWidth = window.innerWidth
-          this.visHeight = this.widthBreak ? 578 : 350
-        }, 200)
-      )
-    })
-    this.fetchData(this.regionId, this.range)
-    this.mounted = true
   },
 
   methods: {
