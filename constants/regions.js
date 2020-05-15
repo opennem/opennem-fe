edadde2b789119d57a63b24f6839281242d98535
Steven Tan <steven@steventan.com.au>
2020-05-15 12:19:27 +1000

    add state colours to constant

--- constants/regions.js
+++ constants/regions.js
@@ -2,41 +2,49 @@ export default [
   {
     id: 'all',
     abbr: 'All',
-    label: 'All Regions'
+    label: 'All Regions',
+    colour: '#e34a33'
   },
   {
     id: 'nem',
     abbr: 'NEM',
-    label: 'NEM'
+    label: 'NEM',
+    colour: '#e34a33'
   },
   {
     id: 'nsw1',
     abbr: 'NSW',
-    label: 'New South Wales'
+    label: 'New South Wales',
+    colour: '#88CEEB'
   },
   {
     id: 'qld1',
     abbr: 'Qld',
-    label: 'Queensland'
+    label: 'Queensland',
+    colour: '#800000'
   },
   {
     id: 'sa1',
     abbr: 'SA',
-    label: 'South Australia'
+    label: 'South Australia',
+    colour: '#FFD800'
   },
   {
     id: 'tas1',
     abbr: 'Tas',
-    label: 'Tasmania'
+    label: 'Tasmania',
+    colour: '#006A4E'
   },
   {
     id: 'vic1',
     abbr: 'Vic',
-    label: 'Victoria'
+    label: 'Victoria',
+    colour: '#020080'
   },
   {
     id: 'wa1',
     abbr: 'WA',
-    label: 'Western Australia'
+    label: 'Western Australia',
+    colour: '#000000'
   }
 ]
