972faa4c488ba74609e8a078ae62066c0430cded
Steven Tan <steven@steventan.com.au>
2020-05-15 12:19:57 +1000

    create placeholder all regions page

---
+++ components/Energy/RegionsTable.vue
@@ -0,0 +1,38 @@
+<template>
+  <table class="table is-fullwidth is-narrow is-bordered">
+    <thead>
+      <tr>
+        <th>States</th>
+        <th>Energy</th>
+        <th>Price</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>New South Wales</td>
+        <td/>
+        <td/>
+      </tr>
+      <tr>
+        <td>Queensland</td>
+        <td/>
+        <td/>
+      </tr>
+      <tr>
+        <td>South Australia</td>
+        <td/>
+        <td/>
+      </tr>
+      <tr>
+        <td>Tasmania</td>
+        <td/>
+        <td/>
+      </tr>
+      <tr>
+        <td>Victoria</td>
+        <td/>
+        <td/>
+      </tr>
+    </tbody>
+  </table>
+</template>
---
+++ pages/energy/_region/view/regions.vue
@@ -0,0 +1,32 @@
+<template>
+  <section class="container">
+    <regions-table />
+
+  </section>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex'
+import RegionsTable from '@/components/Energy/RegionsTable.vue'
+
+export default {
+  layout: 'main',
+  components: {
+    RegionsTable
+  },
+  created() {
+    this.doGetAllRegions()
+  },
+  methods: {
+    ...mapActions({
+      doGetAllRegions: 'region/doGetAllRegions'
+    })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin-top: 3rem;
+}
+</style>
