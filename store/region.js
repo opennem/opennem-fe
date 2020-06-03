2d19dc56253197c76ae307401bf3434502e17d13
Steven Tan <steven@steventan.com.au>
2020-06-03 17:39:28 +1000

    add additional price dataset stores

--- store/region.js
+++ store/region.js
@@ -24,6 +24,8 @@ export const state = () => ({
   emissionVolDataset: [],
   emissionIntDataset: [],
   priceDataset: [],
+  priceAbove300Dataset: [],
+  priceBelow0Dataset: [],
   temperatureDataset: [],
   regions: Regions,
   hasEmissions: false
@@ -35,6 +37,8 @@ export const getters = {
   emissionVolDataset: state => state.emissionVolDataset,
   emissionIntDataset: state => state.emissionIntDataset,
   priceDataset: state => state.priceDataset,
+  priceAbove300Dataset: state => state.priceAbove300Dataset,
+  priceBelow0Dataset: state => state.priceBelow0Dataset,
   temperatureDataset: state => state.temperatureDataset,
   regions: state => state.regions,
   hasEmissions: state => state.hasEmissions
@@ -55,6 +59,13 @@ export const mutations = {
   },
   priceDataset(state, priceDataset) {
     state.priceDataset = priceDataset
+  },
+  priceAbove300Dataset(state, priceAbove300Dataset) {
+    state.priceAbove300Dataset = priceAbove300Dataset
+  },
+  priceBelow0Dataset(state, priceBelow0Dataset) {
+    console.log(priceBelow0Dataset)
+    state.priceBelow0Dataset = priceBelow0Dataset
   },
   temperatureDataset(state, temperatureDataset) {
     state.temperatureDataset = temperatureDataset
@@ -204,10 +215,26 @@ export const actions = {
               'priceDataset',
               transformDataset(Regions, obj, 'price', true)
             )
+            commit(
+              'priceAbove300Dataset',
+              transformDataset(Regions, obj, 'price.above300')
+            )
+            commit(
+              'priceBelow0Dataset',
+              transformDataset(Regions, obj, 'price.below0')
+            )
           } else {
             commit(
               'priceDataset',
               transformDataset(Regions, obj, '_volWeightedPrice')
+            )
+            commit(
+              'priceAbove300Dataset',
+              transformDataset(Regions, obj, '_volWeightedPriceAbove300')
+            )
+            commit(
+              'priceBelow0Dataset',
+              transformDataset(Regions, obj, '_volWeightedPriceBelow0')
             )
           }
 
