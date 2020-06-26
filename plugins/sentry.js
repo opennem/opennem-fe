74385330dd1ee93307be8048c94d4cd174f3aaf2
Steven Tan <steven@steventan.com.au>
2020-06-26 12:00:26 +1000

    enable sentry only for prod

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import * as Sentry from '@sentry/browser'
 import { Vue as VueIntegration } from '@sentry/integrations'
 
-Sentry.init({
-  dsn:
-    'https://c89c945a3c14478f9df55a65ddb1fbae@o402615.ingest.sentry.io/5265226',
-  integrations: [new VueIntegration({ Vue, attachProps: true })]
-})
+const host = window.location.host
+if (host === 'opennem.org.au') {
+  Sentry.init({
+    dsn:
+      'https://c89c945a3c14478f9df55a65ddb1fbae@o402615.ingest.sentry.io/5265226',
+    integrations: [new VueIntegration({ Vue, attachProps: true })]
+  })
+}
