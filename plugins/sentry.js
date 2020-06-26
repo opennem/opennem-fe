a51be39c94af6d914f4a9403708e8dd1c30a1d6f
Steven Tan <steven@steventan.com.au>
2020-06-26 16:55:50 +1000

    enable sentry for dev

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/browser'
 import { Vue as VueIntegration } from '@sentry/integrations'
 
 const host = window.location.host
-if (host === 'opennem.org.au') {
+if (host !== 'localhost:3000') {
   Sentry.init({
     dsn:
       'https://c89c945a3c14478f9df55a65ddb1fbae@o402615.ingest.sentry.io/5265226',
