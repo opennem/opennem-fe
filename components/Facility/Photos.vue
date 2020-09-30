c58b245615409f24d41a56d1b2030778107835d2
Steven Tan <steven@steventan.com.au>
2020-09-30 12:04:41 +1000

    add image attribution

--- components/Facility/Photos.vue
+++ components/Facility/Photos.vue
@@ -1,13 +1,21 @@
 <template>
   <figure>
+    <v-popover 
+      class="wiki-link-text" 
+      placement="auto">
+      <i class="fal fa-info-circle"/>
+      <template slot="popover">
+        Photo by <a :href="photo.author_link">{{ photo.author }}</a>
+      </template>
+    </v-popover>
+
     <img 
       :src="photo.photo_url" 
       :alt="`${name} facility`"
       :style="{ 'height': ratio > 2 ? '180px' : 'auto'}"
     >
-    <!-- <figcaption>
-      1 of {{ ratio }}
-    </figcaption> -->
+
+    
   </figure>
 </template>
 
@@ -46,11 +54,27 @@ export default {
 <style lang="scss" scoped>
 $radius: 0.5rem;
 
-img {
-  border-radius: $radius;
-  max-height: 400px;
-  margin: 0 auto;
-  display: block;
-  object-fit: cover;
+figure {
+  position: relative;
+  img {
+    border-radius: $radius;
+    max-height: 400px;
+    margin: 0 auto;
+    display: block;
+    object-fit: cover;
+  }
+
+  .wiki-link-text {
+    position: absolute;
+    bottom: 8px;
+    right: 12px;
+    color: #fff;
+    font-size: 1.2em;
+
+    i {
+      position: relative;
+      top: 2px;
+    }
+  }
 }
 </style>
