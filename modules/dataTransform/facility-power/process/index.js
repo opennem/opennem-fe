380fd9c079fd02589fe7afd560a70ea6383afb3d
Steven Tan <steven@steventan.com.au>
2020-10-21 10:23:43 +1100

    warn if data.length is 0

--- modules/dataTransform/facility-power/process/index.js
+++ modules/dataTransform/facility-power/process/index.js
@@ -2,6 +2,11 @@ import { checkHistoryObject } from '@/services/DataCheck.js'
 import createEmptyDatasets from '@/modules/dataTransform/helpers/createEmptyDatasets.js'
 
 export default function(data, range) {
+  // check if units exists
+  if (data.length === 0) {
+    console.warn('There are no units for this facility.', data)
+  }
+
   // map to existing structure and filter out null histories
   const units = data
     .map(d => {
