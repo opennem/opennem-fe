8ad6ea581159d7d3483a26c490db6cc4ef727b39
Steven Tan <steven@steventan.com.au>
2020-11-04 12:07:56 +1100

    add error.vue

---
+++ layouts/error.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    redirecting...
+  </section>
+</template>
+
+<script>
+export default {
+  created() {
+    this.$router.push({
+      path: `${this.$route.path}/`
+    })
+  }
+}
+</script>
