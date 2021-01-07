86501cc36bdd36cac1eedbed079125dcabc0763a
Steven Tan <steven@steventan.com.au>
2021-01-07 11:01:30 +1100

    refactor process.js

---
+++ modules/dataTransform/energy/process/combineMultipleResponses.js
@@ -0,0 +1,33 @@
+import _cloneDeep from 'lodash.clonedeep'
+
+export default function(responses) {
+  // combine multiple periods
+  const firstResponse = responses[0]
+  const data = _cloneDeep(firstResponse)
+
+  responses.forEach((res, i) => {
+    if (i > 0) {
+      res.forEach(r => {
+        const find = data.find(d => d.id === r.id)
+        if (find) {
+          find.history.last = r.history.last
+          find.history.data = [...find.history.data, ...r.history.data]
+        } else {
+          // console.warn(`${r.id} is missing from other responses`)
+          // create missing obj
+          const missing = _cloneDeep(r)
+          const newStart = firstResponse[0].history.start
+          const newData = Array.from(
+            Array(firstResponse[0].history.data.length),
+            () => null
+          )
+          missing.history.start = newStart
+          missing.history.data = [...newData, ...r.history.data]
+          data.push(missing)
+        }
+      })
+    }
+  })
+
+  return data
+}
--- modules/dataTransform/energy/process/index.js
+++ modules/dataTransform/energy/process/index.js
@@ -1,9 +1,9 @@
-import _cloneDeep from 'lodash.clonedeep'
 import PerfTime from '@/plugins/perfTime.js'
 import { EMISSIONS, MARKET_VALUE } from '@/constants/data-types'
 import createEmptyDatasets from '@/modules/dataTransform/helpers/createEmptyDatasets.js'
 import parseAndCheckData from './parseAndCheckData.js'
 import flattenAndInterpolate from './flattenAndInterpolate.js'
+import combineMultipleResponses from './combineMultipleResponses.js'
 import {
   getFuelTechWithTypeDomains,
   getFuelTechInOrder,
@@ -17,32 +17,8 @@ const perfTime = new PerfTime()
 
 export default function(responses) {
   perfTime.time()
-  // combine multiple periods
-  const firstResponse = responses[0]
-  const data = _cloneDeep(firstResponse)
-  responses.forEach((res, i) => {
-    if (i > 0) {
-      res.forEach(r => {
-        const find = data.find(d => d.id === r.id)
-        if (find) {
-          find.history.last = r.history.last
-          find.history.data = [...find.history.data, ...r.history.data]
-        } else {
-          // console.warn(`${r.id} is missing from other responses`)
-          // create missing obj
-          const missing = _cloneDeep(r)
-          const newStart = firstResponse[0].history.start
-          const newData = Array.from(
-            Array(firstResponse[0].history.data.length),
-            () => null
-          )
-          missing.history.start = newStart
-          missing.history.data = [...newData, ...r.history.data]
-          data.push(missing)
-        }
-      })
-    }
-  })
+
+  const data = combineMultipleResponses(responses)
 
   const {
     dataAll,
