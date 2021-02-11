d4316eb58269dce88ddc9275acdd198fab4e0a1a
Steven Tan <steven@steventan.com.au>
2021-02-11 14:43:15 +1100

    use region-energy rollup

--- data/parse/facility-energy/rollUp/index.js
+++ data/parse/facility-energy/rollUp/index.js
@@ -1,6 +1,6 @@
 import timeGroups from '@/data/helpers/time-groups'
-import powerRollUp from './powerRollUp'
-import energyRollUp from './energyRollUp'
+import powerRollUp from '@/data/parse/region-energy/rollUp/powerRollUp'
+import energyRollUp from '@/data/parse/region-energy/rollUp/energyRollUp'
 
 export default function({ domains, datasetFlat, interval, isEnergyType }) {
   return timeGroups({
