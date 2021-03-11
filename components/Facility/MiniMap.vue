432fbcd36e14df0e6b4a5705d83486ac50de4ed5
Steven Tan <steven@steventan.com.au>
2021-03-11 15:42:59 +1100

    Update MiniMap.vue

--- components/Facility/MiniMap.vue
+++ components/Facility/MiniMap.vue
@@ -1,7 +1,7 @@
 <template>
   <section
     :style="{ height: coordinates ? `${height}px` : '' }"
-    :class="{ 'full-screen': isFullScreen}"
+    :class="{ 'full-screen': isFullScreen }"
     class="mapbox"
     @click.self="handleExpandClick">
     <client-only>
@@ -320,6 +320,7 @@ export default {
     border-radius: 20px;
   }
   ::v-deep .mapboxgl-ctrl-attrib-button {
+    background-repeat: no-repeat;
     background-color: transparent;
     background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg width='20' height='20' viewBox='0 0 512 512' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='white' d='M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-36 344h12V232h-12c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12h48c6.627 0 12 5.373 12 12v140h12c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12h-72c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12zm36-240c-17.673 0-32 14.327-32 32s14.327 32 32 32 32-14.327 32-32-14.327-32-32-32z'/%3E%3C/svg%3E");
 
@@ -327,6 +328,7 @@ export default {
     height: 20px;
   }
   ::v-deep .mapboxgl-ctrl-attrib-button[aria-pressed='true'] {
+    background-repeat: no-repeat;
     background-image: url("data:image/svg+xml;charset=utf-8,%3Csvg width='20' height='20' viewBox='0 0 512 512' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath fill='black' d='M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-36 344h12V232h-12c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12h48c6.627 0 12 5.373 12 12v140h12c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12h-72c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12zm36-240c-17.673 0-32 14.327-32 32s14.327 32 32 32 32-14.327 32-32-14.327-32-32-32z'/%3E%3C/svg%3E");
   }
   ::v-deep .mapboxgl-ctrl.mapboxgl-ctrl-attrib {
