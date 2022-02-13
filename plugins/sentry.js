61700fdb503f69a837c5c240fc8139ff8444ab0b
Steven <steven@steventan.com.au>
2022-02-13 12:24:52 +1100

    chore: upgrade sentry packages (#74)

--- plugins/sentry.js
+++ plugins/sentry.js
@@ -1,14 +1,30 @@
 import Vue from 'vue'
-import * as Sentry from '@sentry/browser'
-import { Vue as VueIntegration } from '@sentry/integrations'
+
+import * as Sentry from '@sentry/vue'
+import { BrowserTracing } from '@sentry/tracing'
 
 if (typeof window !== 'undefined') {
   const host = window.location.host
   if (host === 'opennem.org.au') {
     Sentry.init({
+      Vue,
       dsn:
         'https://c89c945a3c14478f9df55a65ddb1fbae@o402615.ingest.sentry.io/5265226',
-      integrations: [new VueIntegration({ Vue, attachProps: true })]
+      integrations: [
+        new BrowserTracing({
+          routingInstrumentation: Sentry.vueRouterInstrumentation(router),
+          tracingOrigins: [
+            'localhost',
+            'opennem.org.au',
+            'dev.opennem.org.au',
+            /^\//
+          ]
+        })
+      ],
+      // Set tracesSampleRate to 1.0 to capture 100%
+      // of transactions for performance monitoring.
+      // We recommend adjusting this value in production
+      tracesSampleRate: 1.0
     })
   }
 }
