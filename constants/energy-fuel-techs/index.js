5506908fafd9a564d98c06b0a2ec9dd34e354098
Steven <steven@steventan.com.au>
2023-08-03 12:20:00 +1000

    remove solar residual group

--- constants/energy-fuel-techs/index.js
+++ constants/energy-fuel-techs/index.js
@@ -3,7 +3,6 @@ import * as DefaultGroup from './group-default.js'
 import * as SimplifiedGroup from './group-simplified.js'
 import * as FlexibilityGroup from './group-flexibility.js'
 import * as RenewableFossilGroup from './group-renewable-fossil.js'
-import * as SolarResidualGroup from './group-solar-residual.js'
 import * as CoalGasRenewablesGroup from './group-coal-gas-renewables.js'
 import * as VreResidualGroup from './group-vre-residual.js'
 
@@ -12,7 +11,6 @@ export const GROUP_SIMPLIFIED = 'Simplified'
 export const GROUP_COAL_GAS_RENEWABLES = 'Coal/Gas/Renewables'
 export const GROUP_FLEXIBILITY = 'Flexibility'
 export const GROUP_RENEWABLE_FOSSIL = 'Renewable/Fossil'
-export const GROUP_SOLAR_RESIDUAL = 'Solar/Residual'
 export const GROUP_VRE_RESIDUAL = 'VRE/Residual'
 
 export const Groups = {}
@@ -21,7 +19,6 @@ Groups[GROUP_SIMPLIFIED] = SimplifiedGroup
 Groups[GROUP_COAL_GAS_RENEWABLES] = CoalGasRenewablesGroup
 Groups[GROUP_FLEXIBILITY] = FlexibilityGroup
 Groups[GROUP_RENEWABLE_FOSSIL] = RenewableFossilGroup
-Groups[GROUP_SOLAR_RESIDUAL] = SolarResidualGroup
 Groups[GROUP_VRE_RESIDUAL] = VreResidualGroup
 
 export const ftGroups = Object.keys(Groups)
