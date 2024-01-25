099a205a0437c22c0d6d6118b074a38e0473a640
Steven <steven@steventan.com.au>
2024-01-25 16:46:26 +1100

    Update calculate-carbon-intensity.js

--- data/calculate-carbon-intensity.js
+++ data/calculate-carbon-intensity.js
@@ -1,3 +1,4 @@
+import _includes from 'lodash/includes'
 import * as FT from '@/constants/energy-fuel-techs/group-detailed.js'
 
 function calAverage(isEnergyType, isWemOrAu, dataset) {
@@ -34,31 +35,54 @@ export default function({
   isEnergyType,
   isWemOrAu
 }) {
-  
 
-  const addUp = (data, domain, hasSource) => {
-    // if (isCalculateByGeneration) {
-    //   // only if it's a source AND it's not imports
-    //   if (domain.category === 'source' && domain.fuelTech !== 'imports') {
-    //     return data[domain.id] || 0
-    //   }
-    // } else {
-    //   // it's not a load OR it's exports
-    //   if (domain.category !== 'load' || domain.fuelTech === 'exports') {
-    //     return Math.abs(data[domain.id]) || 0
-    //   }
-    // }
+  console.log('calculate EI', emissionsDomains, powerEnergyDomains, domainPowerEnergy)
 
-    if (hasSource) {
-      // only if it's a source AND it's not imports
-      if (domain.category === 'source' && domain.fuelTech !== 'imports') {
+  const timeCheck = 1696140000000
+  const shouldTimeCheck = false
+
+  /**
+    when viewing generation:
+    sum(all emissions from visible sources except imports) / energy from all of the above
+
+    when viewing consumption:
+    sum(all emissions from visible sources) minus exports / energy from all of the above
+ */
+  const getEmissions = (data, domain) => {
+    if (isCalculateByGeneration) {
+      /** Generation */
+      // return value if category is source and not imports
+      if (domain.category === 'source' && !_includes(domain.id, 'imports')) {
         return data[domain.id] || 0
       }
-      return 0
     } else {
-      // if all the selected domains are loads
-      return Math.abs(data[domain.id]) || 0
+      /** Consumption */
+      // return exports as negative value
+      if (_includes(domain.id, 'exports')) {
+        return -data[domain.id] || 0
+      }
+
+      // return value if category is source
+      if (domain.category === 'source') {
+        return data[domain.id] || 0
+      }
     }
+
+    // else return 0
+    return 0
+  }
+
+  const getPowerEnergy = (data, domain) => {
+    // return value if category is source or exports
+    // if (domain.category === 'source' || _includes(domain.id, 'exports')) {
+    //   return data[domain.id] || 0
+    // }
+    return data[domain.id] || 0
+  }
+
+  const getPowerEnergy2 = (data, domain) => {
+    // test only
+    return data[domain.id] || 0
   }
   
   const batteryDischarging = domainPowerEnergy.find(
@@ -79,6 +103,7 @@ export default function({
       time: d.time,
       _isIncompleteBucket: d._isIncompleteBucket
     }
+
     let totalEmissions = 0,
       totalEmissionsMinusLoads = 0,
       totalPowerEnergy = 0,
@@ -89,16 +114,29 @@ export default function({
     }
 
     emissionsDomains.forEach((domain) => {
-      totalEmissions += addUp(d, domain, hasSource)
+      totalEmissions += getEmissions(d, domain)
+
+      if (shouldTimeCheck && d.time === timeCheck) {
+        console.log(`emissions,${domain.id},${getEmissions(d, domain)}`)
+      }
 
       if (domain.category !== 'load') {
-        totalEmissionsMinusLoads += addUp(d, domain, hasSource)
+        totalEmissionsMinusLoads += getEmissions(d, domain)
       }
     })
 
     powerEnergyDomains.forEach((domain) => {
-      totalPowerEnergy += addUp(d, domain, hasSource)
+      totalPowerEnergy += getPowerEnergy(d, domain)
+
+      if (shouldTimeCheck && d.time === timeCheck) {
+        console.log(`power,${domain.id},${getPowerEnergy2(d, domain)}`)
+      }
     })
+
+    if (shouldTimeCheck && d.time === timeCheck) {
+      console.log('totalEmissions', totalEmissions)
+      console.log('totalPowerEnergy', totalPowerEnergy)
+    }
 
     const totalPowerEnergyMinusBatteryDischarging =
       totalPowerEnergy - totalBatteryDischarging
@@ -120,7 +158,11 @@ export default function({
         ei = ei * 12
       }
     }
-    
+
+    if (shouldTimeCheck && d.time === timeCheck) {
+      console.log('EI', ei)
+    }
+
     const isValidEI = Number.isFinite(ei)
 
     if (ei < 0 || ei > 1500) {
@@ -141,7 +183,11 @@ export default function({
   )
   const averageEmissions = hasSource 
     ? sumEmissionsMinusLoads / dataset.length 
-    : sumEmissions / dataset.length
+    : Math.abs(sumEmissions / dataset.length)
+  
+  if (averageEmissions < 0) {
+    console.log(`averageEmissions < 0: ${averageEmissions}`)
+  }
 
   return {
     emissionIntensityData: dataset,
