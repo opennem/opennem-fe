8633433ed722b5755b55489a2cded98f983f4958
Steven <steven@steventan.com.au>
2024-06-25 17:49:33 +1000

    Update ga.js

--- static/scripts/ga.js
+++ static/scripts/ga.js
@@ -2,4 +2,4 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-FYYXTDCEND');
+gtag('config', 'G-461CNN34M4'); // opennem - G-FYYXTDCEND
