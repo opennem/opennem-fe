1571eb7fc83499bed4898c797c80ad7c60b32d29
Steven Tan <steven@steventan.com.au>
2025-04-15 10:48:58 +1000

    add plausible script

--- nuxt.config.js
+++ nuxt.config.js
@@ -197,7 +197,12 @@ module.exports = {
     ],
     script: [
       { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-FYYXTDCEND"' },
-      { src: '/scripts/ga.js' }
+      { src: '/scripts/ga.js' },
+      {
+        defer: true,
+        'data-domain': 'openelectricity.org.au',
+        src: 'https://plausible.io/js/script.js'
+      }
     ]
   },
 
